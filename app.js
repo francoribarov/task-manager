f022f1530f2a050a64a3f1fc96489589544773b1
francoribarov <francoribarov@gmail.com>
2023-04-02 22:47:24 -0300

    connected to the front-end

--- app.js
+++ app.js
@@ -5,7 +5,9 @@ const tasks = require('./routes/tasks.js')
 const connectDb = require('./db/connect')
 require('dotenv').config()
 
+app.use(express.static('./public'))
 app.use(express.json())
+
 
 
 app.use('/api/v1/tasks', tasks)
