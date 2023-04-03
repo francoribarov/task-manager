06e1677c9a30972cd50278c84425e5e6c539e8e0
francoribarov <francoribarov@gmail.com>
2023-04-02 22:05:40 -0300

    created App.js, connected to mongo

---
+++ app.js
@@ -0,0 +1,24 @@
+require('./db/connect')
+const express = require('express')
+const app = express()
+const tasks = require('./routes/tasks.js')
+const connectDb = require('./db/connect')
+require('dotenv').config()
+
+app.use(express.json())
+
+
+app.use('/api/v1/tasks', tasks)
+
+const port = 3000
+
+const start = async () => {
+    try {
+        await connectDb(process.env.MONGO_URI)
+        app.listen(port, console.log(`port is listening in ${port}`))
+    } catch (error){
+        console.log('error')
+    }
+}
+
+start()
